dbecab4f39c316b87af0b447d153264dea17e39d
fasbrothers <nigmatovsanjar00@gmail.com>
2021-09-20 20:22:53 +0500

    make some changes

---
+++ src/components/AppRouter.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { CHAT_ROUTE, LOGIN_ROUTE } from "../util/const";
+import { privateRoutes, publicRoutes } from "../routes";
+
+function AppRouter() {
+  const user = false;
+  return user ? (
+    <Switch>
+      {privateRoutes.map(({ path, Component }) => 
+        <Route path={path} component={Component} exact={true} />
+      )}
+      <Redirect to={CHAT_ROUTE} />
+    </Switch>
+  ) : (
+    <Switch>
+      {publicRoutes.map(({ path, Component }) => 
+        <Route path={path} component={Component} exact={true} />
+      )}
+      <Redirect to={LOGIN_ROUTE} />
+    </Switch>
+  );
+}
+
+export default AppRouter;
