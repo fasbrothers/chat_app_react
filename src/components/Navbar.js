af3a2736f3eb80f18bef7c9c737a54ad24a79ded
fasbrothers <nigmatovsanjar00@gmail.com>
2021-09-26 15:40:08 +0500

    make navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { Grid, Button, Toolbar, AppBar } from "@material-ui/core";
+import { NavLink } from "react-router-dom";
+import { LOGIN_ROUTE } from "../util/const";
 
 function Navbar() {
-  return <div>navbar</div>;
+  const user = false;
+  return (
+    <AppBar className="app" position="static">
+      <Toolbar variant="dense">
+        <h3>iChat</h3>
+        <Grid container justifyContent={"flex-end"}>
+          {user ? (
+            <Button color="inherit">Quit</Button>
+          ) : (
+            <NavLink to={LOGIN_ROUTE}>
+              <Button variant="outlined">Login</Button>
+            </NavLink>
+          )}
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
