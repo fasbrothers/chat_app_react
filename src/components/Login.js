f7f6cf0827298d36a9d51fb954b2d6fcbec33079
fasbrothers <nigmatovsanjar00@gmail.com>
2021-09-27 10:06:30 +0500

    make some changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,37 @@
-import React from "react";
+import { Container, Grid, Box, Button } from "@material-ui/core";
+import React, { useContext } from "react";
+import { Context } from "../index";
+import firebase from "firebase";
 
 function Login() {
+  const { auth } = useContext(Context);
+  const login = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const { user } = await auth.signInWithPopUp(provider);
+    console.log(user);
+  };
   return (
-    <div>
-      <h2>Login</h2>
-    </div>
+    <Container>
+      <Grid
+        container
+        alignItems={"center"}
+        justify={"center"}
+        style={{ height: window.innerHeight - 50 }}
+      >
+        <Grid
+          style={{ width: 400, backgroundColor: "lightgray" }}
+          container
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Box p={5}>
+            <Button onClick={login} variant="outlined">
+              Log In With Google
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'
+import React, { createContext } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./App.css";
+// import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAj5SPJLOooYRaNcBaTshjoGWWssCTgVLw",
+  authDomain: "ichat-app-4872d.firebaseapp.com",
+  projectId: "ichat-app-4872d",
+  storageBucket: "ichat-app-4872d.appspot.com",
+  messagingSenderId: "548653304620",
+  appId: "1:548653304620:web:231858a0223a0baf713a89",
+  measurementId: "G-E8NMCDG5MM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const Context = createContext(null);
+const auth = app.auth();
+const firestore = app.firestore;
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Context.Provider value={{ auth, firestore }}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Context.Provider>,
+  document.getElementById("root")
 );
